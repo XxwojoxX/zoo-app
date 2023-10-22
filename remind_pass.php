ce4f60725c02a84fcddd98b6cdcf899d26dc87ca
Wojciech Aleksander Gabryś <wojciech.gabrys@student.po.edu.pl>
2023-10-22 16:40:16 +0200

    wysylanie maili

--- remind_pass.php
+++ remind_pass.php
@@ -2,19 +2,25 @@
 	session_start();
 ?>
 
-<html>
-    <head>
-        <link rel="stylesheet" href="styles/style.css">
-        <link rel="stylesheet" href="styles/media.css">
+<html lang="en">
 
-        <script src="scripts/Fetch_API.js"></script>
-        <script src="scripts/cookie.js"></script>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<head>
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/form.css">
+    <link rel="stylesheet" href="styles/media.css">
+    <link rel="stylesheet" href="styles/media_form.css">
 
-        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
-        <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <script src="scripts/Fetch_API.js"></script>
+    <script src="scripts/autohide.js"></script>
+    <script src="scripts/cookie.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-        <script>
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+
+    <title>ZOO</title>
+
+    <script>
         document.addEventListener("DOMContentLoaded", function () {
             var cookieMessage = document.getElementById("cookie-message");
             var acceptCookieButton = document.getElementById("accept-cookie");
@@ -30,13 +36,40 @@
             }
         });
     </script>
-    </head>
+</head>
 
-    <body>
+<body>
+    <div class="home">
+
         <nav></nav>
 
-        <h1>STRONA W BUDOWIE</h1>
+        <form class="form" id="remind-form" action="PHP/remind_password.php" method="POST" name="remind_form" autocomplete="off">
+            <?php
+            if (isset($_GET['success']) && $_GET['success'] == 1) {
+                echo '<div class="remind-success" id="remind-success">
+                                        <h2>wyslano wiadomosc na podany adres email</h2>
+                                    </div>';
+            }
+            ?>
+
+            <p class="title">Remind password</p>
+            <p class="message">Podaj nazwe uzytkownika oraz email</p>
+
+            <label>
+                <input required="" placeholder="" type="text" class="input" name="nick">
+                <span>username</span>
+            </label>
+
+            <label>
+                <input required="" placeholder="" type="text" class="input" name="email">
+                <span>Email</span>
+            </label>
+
+            <button class="submit">Submit</button>
+        </form>
 
         <footer></footer>
-    </body>
+    </div>
+</body>
+
 </html>
