5a17132c6db638354266dcda26d9792cdb3eed4e
Wojciech Aleksander Gabryś <wojciech.gabrys@student.po.edu.pl>
2023-12-05 01:27:02 +0100

    style sklepu i filtrow

--- shop.php
+++ shop.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+
+if (!isset($_SESSION['userName'])) {
+    header("Location: login_form.php");
+}
 ?>
 
 <html>
@@ -34,6 +38,13 @@ session_start();
             </ul>
 
             <div class="log-reg">
+            <button data-quantity="0" class="btn-cart">
+          <svg class="icon-cart" viewBox="0 0 24.38 30.52" height="30.52" width="24.38" xmlns="http://www.w3.org/2000/svg">
+            <title>icon-cart</title>
+            <path transform="translate(-3.62 -0.85)" d="M28,27.3,26.24,7.51a.75.75,0,0,0-.76-.69h-3.7a6,6,0,0,0-12,0H6.13a.76.76,0,0,0-.76.69L3.62,27.3v.07a4.29,4.29,0,0,0,4.52,4H23.48a4.29,4.29,0,0,0,4.52-4ZM15.81,2.37a4.47,4.47,0,0,1,4.46,4.45H11.35a4.47,4.47,0,0,1,4.46-4.45Zm7.67,27.48H8.13a2.79,2.79,0,0,1-3-2.45L6.83,8.34h3V11a.76.76,0,0,0,1.52,0V8.34h8.92V11a.76.76,0,0,0,1.52,0V8.34h3L26.48,27.4a2.79,2.79,0,0,1-3,2.44Zm0,0"></path>
+          </svg>
+          <span class="quantity"></span>
+        </button>
                 <?php
                 if (isset($_SESSION['userName'])) {
                     $userName = $_SESSION['userName'];
@@ -91,11 +102,11 @@ session_start();
 
             <label for="sort" id="sort-label">Sortuj:</label>
             <select id="sort">
+                <option value="default" selected>Domyślnie</option>
                 <option value="price-asc">Cena rosnąco</option>
                 <option value="price-desc">Cena malejąco</option>
-                <option value="alphabetical-asc" selected>Alfabetycznie A-Z</option>
+                <option value="alphabetical-asc">Alfabetycznie A-Z</option>
                 <option value="alphabetical-desc">Alfabetycznie Z-A</option>
-                <!-- Dodaj więcej opcji sortowania -->
             </select>
 
             <label for="category" style="display: none">filtr4</label for="category">
@@ -105,7 +116,7 @@ session_start();
                 <button class="filter-btn" type="checkbox">opcja3</button>
             </div>
 
-            <button id="filter-btn">Zastosuj</button>
+            <button class="filter-btn">Zastosuj</button>
         </div>
 
         <div class="shop-container">
@@ -119,8 +130,8 @@ session_start();
                     echo '<div class="flip-card-inner">';
                     echo '<div class="flip-card-front">';
                     echo '<img src="' . $product['image'] . '" style="width: 75%; height: 75%; border-radius: 20px; margin-top: 30px;">';
-                    echo '<p>' . $product['name'] . '</p>';
-                    echo '<p>' . $product['price'] . '</p>';
+                    echo '<p id="product-name">' . $product['name'] . '</p>';
+                    echo '<p>' . $product['price'] . ' zł</p>';
                     echo '</div>';
                     echo '<div class="flip-card-back">';
                     echo '<img src="' . $product['image'] . '" style="width: 75%; height: 75%; border-radius: 20px; margin-top: 30px;">';
